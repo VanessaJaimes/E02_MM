7928c5646de0150e8ae82af05d1809c399d82717
Marin28c1997 <96443496+Marin28c1997@users.noreply.github.com>
2023-02-06 20:39:27 -0500

    update index

--- js/back.js
+++ js/back.js
@@ -4,10 +4,27 @@ document.getElementById("updateProgress").addEventListener("click", function(){
     document.getElementById("myProgressBar").style.width = progress + "%";
     
     if (progress < 40) {
-      document.getElementById("myProgressBar").className = "progress-bar progress-bar-danger";
+      document.getElementById("myProgressBar").className = "progress-bar progress-bar-striped progress-bar-animated bg-danger";
     } else if (progress < 70) {
-      document.getElementById("myProgressBar").className = "progress-bar progress-bar-warning";
+      document.getElementById("myProgressBar").className = "progress-bar progress-bar-striped progress-bar-animated bg-warning";
     } else {
-      document.getElementById("myProgressBar").className = "progress-bar progress-bar-success";
+      document.getElementById("myProgressBar").className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
     }
   });
+
+
+  document.getElementById("updateProgresss").addEventListener("click", function(){
+    var progress = Math.floor(Math.random() * 101);
+    
+    document.getElementById("myProgressBars").style.width = progress + "%";
+    
+    if (progress < 40) {
+      document.getElementById("myProgressBars").className = "progress-bar progress-bar-striped progress-bar-animated bg-danger";
+    } else if (progress < 70) {
+      document.getElementById("myProgressBars").className = "progress-bar progress-bar-striped progress-bar-animated bg-warning";
+    } else {
+      document.getElementById("myProgressBars").className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
+    }
+  });
+
+  
