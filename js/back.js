490b24692b56ac78494de70f44a3a5f173697cd4
David Ospina <J_do_o13@hotmail.com>
2023-02-07 01:44:05 -0500

    add functions, buy, percent, alert
    se añadieron funciones para comprobar y almacenar las compras realizadas, calcular los porcentajes de ocupación de las bodegas y mostrar los datos en la interfaz

--- js/back.js
+++ js/back.js
@@ -1,18 +1,23 @@
-document.getElementById("updateProgress").addEventListener("click", function () {
-  var progress = Math.floor(Math.random() * 101);
-
-  document.getElementById("myProgressBar").style.width = progress + "%";
-
-  if (progress < 40) {
+function Calc_per(percent, percent1) {
+  document.getElementById("myProgressBar").style.width = percent + "%";
+  if (percent < 40) {
     document.getElementById("myProgressBar").className = "progress-bar progress-bar-striped progress-bar-animated bg-danger";
-  } else if (progress < 70) {
+  } else if (percent < 70) {
     document.getElementById("myProgressBar").className = "progress-bar progress-bar-striped progress-bar-animated bg-warning";
   } else {
     document.getElementById("myProgressBar").className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
   }
-});
 
-
+  document.getElementById("myProgressBar1").style.width = percent1 + "%";
+  if (percent1 < 40) {
+    document.getElementById("myProgressBar1").className = "progress-bar progress-bar-striped progress-bar-animated bg-danger";
+  } else if (percent1 < 70) {
+    document.getElementById("myProgressBar1").className = "progress-bar progress-bar-striped progress-bar-animated bg-warning";
+  } else {
+    document.getElementById("myProgressBar1").className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
+  }
+}
+/*
 document.getElementById("updateProgresss").addEventListener("click", function () {
   var progress = Math.floor(Math.random() * 101);
 
@@ -25,19 +30,57 @@ document.getElementById("updateProgresss").addEventListener("click", function ()
   } else {
     document.getElementById("myProgressBars").className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
   }
-});
+});*/
 
 //1 kl = 2,20462 L
 //1 kl = 1000 g
-function Init(){
-  
+function Init() {
+  localStorage.setItem('Warehouse', JSON.stringify([100000, 230000]))
+  localStorage.setItem('Percent', JSON.stringify([100, 100]))
+  Calc_per(JSON.parse(localStorage.getItem('Percent'))[0],
+    JSON.parse(localStorage.getItem('Percent'))[1])
 }
+Init();
 var app = new Vue({
   el: "#app",
   data: {
     Message: "",
+    Warehouse: "0",
+    Weight: "Kilo",
+    Amount: "",
+    asd: 50 + "%",
+    Percent: [100, 100],
+    Sales: [],
+    Whi: [JSON.parse(localStorage.getItem('Warehouse'))[0],
+    JSON.parse(localStorage.getItem('Warehouse'))[1]],
   },
   methods: {
-
+    Buy() {
+      let amount = this.Amount;
+      (this.Weight == "Gramo") ?
+        amount = amount / 1000 :
+        (this.Weight == "Libra") ?
+          amount = amount / 2.20462 :
+          amount = this.Amount
+      if (this.Whi[this.Warehouse] - amount < 0) {
+        this.Message = "cantidad excedida"
+      } else {
+        this.Whi[this.Warehouse] = this.Whi[this.Warehouse] - amount
+        this.Percent[this.Warehouse] = this.Whi[this.Warehouse] * 100 / JSON.parse(localStorage.getItem('Warehouse'))[this.Warehouse]
+        this.Sales.push({
+          Ware: this.Warehouse,
+          Buy: amount,
+        })
+        Calc_per(this.Percent[0], this.Percent[1])
+      }
+    },
+    Alert() {
+      (this.Percent[this.Warehouse] <= 50) ?
+        (this.Percent[this.Warehouse] <= 10) ?
+          this.Message = "el almacenamiento es inferior al 10% para la bodega " + (this.Warehouse + 1) :
+          this.Message = "el almacenamiento es inferior al 50% para la bodega " + (this.Warehouse + 1)
+        :
+        this.Message = ""
+    }
   }
 })
