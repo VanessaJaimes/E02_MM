53b2c1227eac4cf51497e8b29cb2c133498cd0ba
David Ospina <J_do_o13@hotmail.com>
2023-02-06 23:59:08 -0500

    add init

--- js/back.js
+++ js/back.js
@@ -1,30 +1,43 @@
-document.getElementById("updateProgress").addEventListener("click", function(){
-    var progress = Math.floor(Math.random() * 101);
-    
-    document.getElementById("myProgressBar").style.width = progress + "%";
-    
-    if (progress < 40) {
-      document.getElementById("myProgressBar").className = "progress-bar progress-bar-striped progress-bar-animated bg-danger";
-    } else if (progress < 70) {
-      document.getElementById("myProgressBar").className = "progress-bar progress-bar-striped progress-bar-animated bg-warning";
-    } else {
-      document.getElementById("myProgressBar").className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
-    }
-  });
+document.getElementById("updateProgress").addEventListener("click", function () {
+  var progress = Math.floor(Math.random() * 101);
+
+  document.getElementById("myProgressBar").style.width = progress + "%";
+
+  if (progress < 40) {
+    document.getElementById("myProgressBar").className = "progress-bar progress-bar-striped progress-bar-animated bg-danger";
+  } else if (progress < 70) {
+    document.getElementById("myProgressBar").className = "progress-bar progress-bar-striped progress-bar-animated bg-warning";
+  } else {
+    document.getElementById("myProgressBar").className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
+  }
+});
 
 
-  document.getElementById("updateProgresss").addEventListener("click", function(){
-    var progress = Math.floor(Math.random() * 101);
-    
-    document.getElementById("myProgressBars").style.width = progress + "%";
-    
-    if (progress < 40) {
-      document.getElementById("myProgressBars").className = "progress-bar progress-bar-striped progress-bar-animated bg-danger";
-    } else if (progress < 70) {
-      document.getElementById("myProgressBars").className = "progress-bar progress-bar-striped progress-bar-animated bg-warning";
-    } else {
-      document.getElementById("myProgressBars").className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
-    }
-  });
+document.getElementById("updateProgresss").addEventListener("click", function () {
+  var progress = Math.floor(Math.random() * 101);
 
+  document.getElementById("myProgressBars").style.width = progress + "%";
+
+  if (progress < 40) {
+    document.getElementById("myProgressBars").className = "progress-bar progress-bar-striped progress-bar-animated bg-danger";
+  } else if (progress < 70) {
+    document.getElementById("myProgressBars").className = "progress-bar progress-bar-striped progress-bar-animated bg-warning";
+  } else {
+    document.getElementById("myProgressBars").className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
+  }
+});
+
+//1 kl = 2,20462 L
+//1 kl = 1000 g
+function Init(){
   
+}
+var app = new Vue({
+  el: "#app",
+  data: {
+    Message: "",
+  },
+  methods: {
+
+  }
+})
